d48bbc79f6b96cd1b1481937f8125c96f49b51a2
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2022-10-11 23:02:30 +0200

    Update Finite-Sample-Theory.py

--- Algorithms/Finite-Sample-Theory.py
+++ Algorithms/Finite-Sample-Theory.py
@@ -1,5 +1,6 @@
 # Import required packages
 import numpy as np
+from scipy import stats
 from numpy.linalg import inv
 from numpy.linalg import pinv
 
@@ -15,12 +16,15 @@ def OLS_estimator(feature, target, intercept: bool):
         - b = OLS estimator
     """
     # Include intercept if specified
-    if intercept:
-        feature.insert(loc=0, column='intercept', value=1)
+    try:
+        if intercept:
+            feature.insert(loc=0, column='intercept', value=1)
+    except:
+        features = feature
     
     # Transform to numpy array for computational efficiency
     features = feature.to_numpy()
-    target = target.values
+    y = target.values
 
     # Compute inverse of cross-product matrix X'X 
     try:
@@ -30,7 +34,7 @@ def OLS_estimator(feature, target, intercept: bool):
         inverse = pinv(np.matmul(features.T, features))
 
     # Define X'y
-    covariance = np.matmul(features.T, target)
+    covariance = np.matmul(features.T, y)
     b = np.matmul(inverse, covariance)
     return b
 
@@ -51,10 +55,10 @@ def sigma_estimator(feature, target, intercept: bool):
 
     # Transform to numpy array for computational efficiency
     features = feature.to_numpy()
-    target = target.values
+    y = target.values
 
     # Store matrix dimensions
-    n, k = len(target), features.shape[1]
+    n, k = len(y), features.shape[1]
 
     # Compute inverse of cross-product matrix X'X 
     try:
@@ -65,12 +69,53 @@ def sigma_estimator(feature, target, intercept: bool):
 
     # Calculate squared residual vector e'e
     y_hat = np.matmul(features, b)
-    residuals = target - y_hat
+    residuals = y - y_hat
     resid2 = np.dot(residuals.T, residuals)
 
     # Degrees of freedom correction
     resid2 /= n-k
     
-    # Define S^2
+    # Define variance estimator
     s2 = resid2*inverse
     return s2
+
+def individual_test(coef, value, feature, target, intercept: bool, sign: str):
+    """ Tests the statistical significance of regression coefficients
+
+    - Parameters:
+        - coef = Index of the coefficient to test (0 is intercept)
+        - value = Coefficient value to test
+        - feature = Pandas dataframe containing feature matrix
+        - taget = Pandas dataframe containing target vector
+        - intercept = Whether to include an intercept or not
+        - sign = Hypothesis test sign (two.sided, less, great)
+
+    - Output:
+        - Message with results and p-value
+    """
+    # Get matrix dimensions for degrees of freedom
+    features = feature.to_numpy()
+    y = target.values
+    n, k = len(y), features.shape[1]
+    df = n - k
+
+    # Retrieve OLS estimator and variance estimator
+    b = OLS_estimator(feature, target, intercept)
+    s2 = sigma_estimator(feature, target, intercept)
+
+    # Compute test statistic
+    t = (b[coef] - value)/(np.sqrt(s2[coef, coef]))
+    print(b)
+    # Print results depending on sign
+    if sign == 'two.sided':
+        pvalue = 2*stats.t.cdf(t, df)
+    elif sign == 'less':
+        pvalue = stats.t.cdf(t, df)
+    else:
+        pvalue = 1 - stats.t.cdf(t, df)
+    
+    # Check if H0 is rejected
+    if pvalue <= 0.05:
+        print(f'H0 is rejected with p-value {pvalue}')
+    else:
+        print(f'Failed to reject H0 with p-value {pvalue}')
