141c7d783705072dc2c36418917cc7c7d5157d13
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2022-10-13 21:25:09 +0200

    Update Finite-Sample-Theory.py

--- Algorithms/Finite-Sample-Theory.py
+++ Algorithms/Finite-Sample-Theory.py
@@ -119,3 +119,64 @@ def individual_test(coef, value, feature, target, intercept: bool, sign: str):
         print(f'H0 is rejected with p-value {pvalue}')
     else:
         print(f'Failed to reject H0 with p-value {pvalue}')
+        
+def joint_test(coefs, values, feature, target, intercept: bool):
+    """ Tests the joint statistical significance of regression coefficients.
+    Supports only two-sided tests
+
+    - Parameters:
+        - coef = List with indexes of the coefficient to test (0 is intercept)
+        - value = List with coefficient values to test
+        - feature = Pandas dataframe containing feature matrix
+        - taget = Pandas dataframe containing target vector
+        - intercept = Whether to include an intercept or not
+        - sign = Hypothesis sign (two.sided, less, great)
+
+    - Output:
+        - Message with results and p-value
+    """
+    # Get matrix dimensions for degrees of freedom
+    features = feature.to_numpy()
+    y = target.values
+    n, k = len(target), features.shape[1]
+
+    # Retrieve OLS estimator and variance estimator
+    b = OLS_estimator(feature, target, intercept)
+    s2 = sigma_estimator(feature, target, intercept)
+    
+    # Calculate squared residual vector e'e
+    y_hat = np.matmul(feature, b)
+    residuals = y - y_hat
+    resid2 = np.dot(residuals.T, residuals)
+    resid2 /= (n-k)
+    
+    # Define restriction matrices and value vector
+    r = values
+    R = [[0]*(k+1) for i in range(len(r))]
+    for i, j in zip(range(len(r)), coefs):
+        R[i][j] = 1
+    R = np.asarray(R)
+    
+    # Construct test statistic
+    Rbr = np.matmul(R, b) - r
+    
+    # Check matrix invertibility
+    try:
+        XX_inv = np.linalg.inv(np.matmul(feature.T, feature))
+    except:
+        XX_inv = np.linalg.pinv(np.matmul(feature.T, feature))
+    
+    middle = np.matmul(R, np.matmul(XX_inv, R.T))
+    w = np.matmul(np.matmul(Rbr.T, middle), Rbr.T)/len(r)
+    
+    # Retrieve F-statistic
+    F = w/resid2
+    
+    # Compute p-value
+    pvalue = 1 - stats.f.cdf(F, len(r), n - k)
+
+    # Check if H0 is rejected
+    if pvalue <= 0.05:
+        print(f'H0 is rejected with p-value {pvalue}')
+    else:
+        print(f'Failed to reject H0 with p-value {pvalue}')
