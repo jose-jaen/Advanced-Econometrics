9f21368f99ebe6dea1a2b86798fa8300cfbbe46a
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2022-12-28 09:50:47 +0100

    Update Finite-Sample-Theory.py

--- Algorithms/Python Code/Finite-Sample-Theory.py
+++ Algorithms/Python Code/Finite-Sample-Theory.py
@@ -1,185 +1,112 @@
 # Import required packages
+import pandas as pd
 import numpy as np
-from scipy import stats
-from numpy.linalg import inv
-from numpy.linalg import pinv
+from scipy.stats import t
 
+class OLS:
+    """ Retrieves relevant information about OLS estimation
 
-def OLS_estimator(feature, target, intercept: bool):
-    """ Retrieves the OLS estimator for some dataframe
+    - Initialization parameters:
+        - target = Pandas dataframe with target variable
+        - regressors = Pandas dataframe with predictors
+        - intercept = Whether or not to include a constant
 
-    - Parameters:
-        - feature = Pandas dataframe containing feature matrix
-        - taget = Pandas dataframe containing target vector
-        - intercept = Whether to include an intercept or not
-
-    - Output:
-        - b = OLS estimator
+    - Methods:
+        - data_matrices = Target, regressors and inverted cross-matrix data
+        - ols_estimator = Calculates OLS estimator vector
+        - ols_se = Computes standard errors of OLS estimator
+        - ols_t = Retrieves t-statistic for individual significance
+        - ols_F = Obtains F-statistic for joint significance
+        - ols_ci = 5% significance level confidence interval of OLS estimator
+        - ols_summary = Data matrix with all previous methods' results
     """
-    # Include intercept if specified
-    try:
+    def __init__(self, target, regressors, intercept=True):
+        # Assignments
+        self.target = target
+        self.regressors = regressors
+
+        # Include intercept in data matrix
         if intercept:
-            feature.insert(loc=0, column='intercept', value=1)
-    except:
-        features = feature
+            constant = [1]*len(self.target)
+            self.regressors.insert(loc=0, column='intercept', value=constant)
 
-    # Transform to numpy array for computational efficiency
-    features = feature.to_numpy()
-    y = target.values
+    def data_matrices(self):
+        """" Sets up data matrices for modeling
+        """
+        regressors = self.regressors
+        target = self.target
+        regressors, target = regressors.to_numpy(), target.to_numpy()
+        cross_matrix = np.matmul(regressors.T, regressors)
 
-    # Compute inverse of cross-product matrix X'X
-    try:
-        inverse = inv(np.matmul(features.T, features))
-    except:
-        # If X'X is singular, calculate the Moore-Penrose inverse
-        inverse = pinv(np.matmul(features.T, features))
+        # Invert cross matrix if it is nonsingular
+        try:
+            cross_matrix_inv = np.linalg.inv(cross_matrix)
+        except:
+            cross_matrix_inv = np.linalg.pinv(cross_matrix)
 
-    # Define X'y
-    covariance = np.matmul(features.T, y)
-    b = np.matmul(inverse, covariance)
-    return b
+        return regressors, target, cross_matrix_inv
 
+    def ols_estimator(self):
+        """ Retrieves OLS estimator vector
+        """
+        regressors, target, cross_matrix_inv = self.data_matrices()
+        dot_prod = np.dot(regressors.T, target)
+        b = np.matmul(cross_matrix_inv, dot_prod)
+        b = [i.round(4) for i in b]
+        return b
 
-def sigma_estimator(feature, target, intercept: bool):
-    """ Retrieves the variance estimator for an OLS coefficient vector
+    def ols_se(self):
+        """ Retrieves OLS estimator standard errors (assumes homoskedasticity)
+        """
+        b = self.ols_estimator()
+        regressors, target, cross_matrix_inv = self.data_matrices()
+        n, k = len(target), len(b)
+        resid = target - np.matmul(regressors, b)
+        s2 = sum(resid**2)/(n - k)
+        se = np.sqrt(s2*np.diag(cross_matrix_inv))
+        return se
 
-    - Parameters:
-        - feature = Pandas dataframe containing feature matrix
-        - taget = Pandas dataframe containing target vector
-        - intercept = Whether to include an intercept or not
+    def ols_t(self):
+        """ Retrieves t-statistic for individual significance
+        """
+        b = self.ols_estimator()
+        se = self.ols_se()
+        t_stat = np.divide(b, se)
+        n, k = len(target), len(b)
+        return t_stat, 2*t.sf(abs(t_stat), n - k).round(3)
 
-    - Output:
-        - s2 = OLS variance estimator
-    """
-    # Retrieve OLS estimator
-    b = OLS_estimator(feature, target, intercept)
+    def ols_F(self):
+        """ Calculates F-statistic for joint significance
+        """
+        b = self.ols_estimator()
+        regressors, target, cross_matrix_inv = self.data_matrices()
+        n, k = len(target), len(b)
+        resid_u = target - np.matmul(regressors, b)
+        SSR_U = sum(resid_u**2)
+        resid_r = target - [np.mean(target)]*n
+        SSR_R = sum(resid_r**2)
+        F = ((SSR_R - SSR_U)/(k - 1))/(SSR_U/(n - k))
+        return F
 
-    # Transform to numpy array for computational efficiency
-    features = feature.to_numpy()
-    y = target.values
+    def ols_ci(self):
+        """ Computes onfidence intervals with 5% significance level
+        """
+        b = self.ols_estimator()
+        se = self.ols_se()
+        lb, ub = b - se*1.96, b + se*1.96
+        lb = [i.round(3) for i in lb]
+        ub = [j.round(3) for j in ub]
+        return lb, ub
 
-    # Store matrix dimensions
-    n, k = len(y), features.shape[1]
-
-    # Compute inverse of cross-product matrix X'X
-    try:
-        inverse = inv(np.matmul(features.T, features))
-    except:
-        # If X'X is singular, calculate the Moore-Penrose inverse
-        inverse = pinv(np.matmul(features.T, features))
-
-    # Calculate squared residual vector e'e
-    y_hat = np.matmul(features, b)
-    residuals = y - y_hat
-    resid2 = np.dot(residuals.T, residuals)
-
-    # Degrees of freedom correction
-    resid2 /= n - k
-
-    # Define variance estimator
-    s2 = resid2 * inverse
-    return s2
-
-
-def individual_test(coef, value, feature, target, intercept: bool, sign: str):
-    """ Tests the statistical significance of regression coefficients
-
-    - Parameters:
-        - coef = Index of the coefficient to test (0 is intercept)
-        - value = Coefficient value to test
-        - feature = Pandas dataframe containing feature matrix
-        - taget = Pandas dataframe containing target vector
-        - intercept = Whether to include an intercept or not
-        - sign = Hypothesis test sign (two.sided, less, great)
-
-    - Output:
-        - Message with results and p-value
-    """
-    # Get matrix dimensions for degrees of freedom
-    features = feature.to_numpy()
-    y = target.values
-    n, k = len(y), features.shape[1]
-    df = n - k
-
-    # Retrieve OLS estimator and variance estimator
-    b = OLS_estimator(feature, target, intercept)
-    s2 = sigma_estimator(feature, target, intercept)
-
-    # Compute test statistic
-    t = (b[coef] - value) / (np.sqrt(s2[coef, coef]))
-    
-    # Print results depending on sign
-    if sign == 'two.sided':
-        pvalue = 2*(1 - stats.t.cdf(abs(t), df))
-    elif sign == 'less':
-        pvalue = stats.t.cdf(t, df)
-    else:
-        pvalue = 1 - stats.t.cdf(t, df)
-
-    # Check if H0 is rejected
-    if pvalue <= 0.05:
-        print(f'H0 is rejected with p-value {pvalue}')
-    else:
-        print(f'Failed to reject H0 with p-value {pvalue}')
-
-
-def joint_test(coefs, values, feature, target, intercept: bool):
-    """ Tests the joint statistical significance of regression coefficients.
-    Supports only two-sided tests
-
-    - Parameters:
-        - coef = List with indexes of the coefficient to test (0 is intercept)
-        - value = List with coefficient values to test
-        - feature = Pandas dataframe containing feature matrix
-        - taget = Pandas dataframe containing target vector
-        - intercept = Whether to include an intercept or not
-        - sign = Hypothesis sign (two.sided, less, great)
-        
-    - Output:
-        - Message with results and p-value
-    """
-    # Get matrix dimensions for degrees of freedom
-    features = feature.to_numpy()
-    y = target.values
-    n, k = len(target), features.shape[1]
-
-    # Retrieve OLS estimator and variance estimator
-    b = OLS_estimator(feature, target, intercept)
-    s2 = sigma_estimator(feature, target, intercept)
-
-    # Calculate squared residual vector e'e
-    y_hat = np.matmul(feature, b)
-    residuals = y - y_hat
-    resid2 = np.dot(residuals.T, residuals)
-    resid2 /= (n - k)
-
-    # Define restriction matrices and value vector
-    r = values
-    R = [[0] * (k + 1) for i in range(len(r))]
-    for i, j in zip(range(len(r)), coefs):
-        R[i][j] = 1
-    R = np.asarray(R)
-
-    # Construct test statistic
-    Rbr = np.matmul(R, b) - r
-
-    # Check matrix invertibility
-    try:
-        XX_inv = np.linalg.inv(np.matmul(feature.T, feature))
-    except:
-        XX_inv = np.linalg.pinv(np.matmul(feature.T, feature))
-
-    middle = np.matmul(R, np.matmul(XX_inv, R.T))
-    w = np.matmul(np.matmul(Rbr.T, middle), Rbr.T) / len(r)
-
-    # Retrieve F-statistic
-    F = w / resid2
-    print(F)
-    # Compute p-value
-    pvalue = 1 - stats.f.cdf(F, len(r), n - k)
-
-    # Check if H0 is rejected
-    if pvalue <= 0.05:
-        print(f'H0 is rejected with p-value {pvalue}')
-    else:
-        print(f'Failed to reject H0 with p-value {pvalue}')
+    def ols_summary(self):
+        """ Provides summary of OLS estimation
+        """
+        cols = self.regressors.columns
+        b = self.ols_estimator()
+        se = self.ols_se()
+        t, p_value = self.ols_t()
+        lb, ub = self.ols_ci()
+        summary = pd.DataFrame(data=np.column_stack((
+            cols, b, se, t, p_value, lb, ub)),
+            columns=['features', 'coef', 'std err', 't', 'P>|t|', '[0.025', '0.975]'])
+        return summary
