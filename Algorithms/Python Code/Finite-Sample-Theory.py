090a3796374ce91b1fa9310d57ee88981511bae0
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2022-12-29 13:46:45 +0100

    Update Finite-Sample-Theory.py

--- Algorithms/Python Code/Finite-Sample-Theory.py
+++ Algorithms/Python Code/Finite-Sample-Theory.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.stats import t
 
 class OLS:
-    """ Retrieves relevant information about OLS estimation
+    """ Retrieves OLS estimator, standard errors and confidence intervals
 
     - Initialization parameters:
         - target = Pandas dataframe with target variable
@@ -26,14 +26,16 @@ class OLS:
         self.regressors = regressors
 
         # Include intercept in data matrix
-        if intercept:
+        if intercept and 'intercept' not in self.regressors.columns:
             constant = [1]*len(self.target)
             self.regressors.insert(loc=0, column='intercept', value=constant)
 
     def data_matrices(self):
         """" Sets up data matrices for modeling
         """
-        regressors, target = self.regressors.to_numpy(), self.target.to_numpy()
+        regressors = self.regressors
+        target = self.target
+        regressors, target = regressors.to_numpy(), target.to_numpy()
         cross_matrix = np.matmul(regressors.T, regressors)
 
         # Invert cross matrix if it is nonsingular
@@ -60,7 +62,7 @@ class OLS:
         regressors, target, cross_matrix_inv = self.data_matrices()
         n, k = len(target), len(b)
         resid = target - np.matmul(regressors, b)
-        s2 = sum(resid**2)/(n - k)
+        s2 = sum(resid**2)/(n-k)
         se = np.sqrt(s2*np.diag(cross_matrix_inv))
         return se
 
@@ -83,7 +85,7 @@ class OLS:
         SSR_U = sum(resid_u**2)
         resid_r = target - [np.mean(target)]*n
         SSR_R = sum(resid_r**2)
-        F = ((SSR_R - SSR_U)/(k - 1))/(SSR_U/(n - k))
+        F = ((SSR_R - SSR_U)/(k-1))/(SSR_U/(n - k))
         return F
 
     def ols_ci(self):
@@ -108,3 +110,52 @@ class OLS:
             cols, b, se, t, p_value, lb, ub)),
             columns=['features', 'coef', 'std err', 't', 'P>|t|', '[0.025', '0.975]'])
         return summary
+
+
+class OLS_tests(OLS):
+    def __init__(self, target, regressors, intercept=True):
+        super().__init__(
+            target, regressors, intercept
+        )
+
+    def individual_test(self, coef: str, value: float, sign: str):
+        """ Returns outcome of individual significance test
+        """
+        assert sign in ['two.sided', 'less', 'great'], f'Sign argument must be either ' \
+            f"'two.sided', 'less' or 'great' but '{sign}' was introduced"
+        self.coef = coef
+        self.value = value
+        self.sign = sign
+
+        # Set up data matrices
+        regressors, target = self.regressors, self.target
+
+        # Retrieve OLS estimator and variance estimator
+        b = self.ols_estimator()
+        s2 = self.ols_se()
+
+        # Compute degrees of freedom
+        n, k = len(target), len(b)
+        df = n - k
+
+        # Obtain OLS estimate
+        cols = list(regressors.columns)
+        idx = cols.index(self.coef)
+        b_k = b[idx]
+
+        # Calculate test-statistic
+        t_stat = (b_k - self.value)/s2[idx]
+
+        # Print results depending on sign
+        if self.sign == 'two.sided':
+            pvalue = 2*t.sf(abs(t_stat), n - k)
+        elif self.sign == 'less':
+            pvalue = 1 - t.cdf(t_stat, df)
+        else:
+            pvalue = t.cdf(t_stat, df)
+
+        # Check if H0 is rejected
+        if pvalue <= 0.05:
+            print(f'H0 is rejected with p-value {pvalue.round(3)}')
+        else:
+            print(f'Failed to reject H0 with p-value {pvalue.round(3)}')
